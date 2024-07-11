4987b9cbd3763f35532824f34dd581fcae27ec50
Billy Basdras <billbasdras@yahoo.gr>
2024-07-11 16:19:13 +0200

    posts in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function home(){
-        return view('home');
+
+        $posts = Post::query()->orderBy('created_at', 'desc')->get();
+
+        foreach($posts as $post){
+            $user = User::find($post->user_id);
+            $post->user = $user;
+        }
+
+        return view('home', ['posts' => $posts]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,33 @@
 <x-layout>
     <section class="">
         <h1 class="text-2xl">Homepage</h1>
-    </section>  
+
+    </section>
+
+    <section class="flex flex-row flex-wrap gap-4">
+        @foreach($posts as $post)
+            <div class="grid gap-4 p-4 border shadow-md max-w-[380px]">
+                <h2 class="text-xl"> {{ $post->title }} </h2>
+                <img src="{{ url( 'storage/' . $post->image_path) }}" alt="post image">
+                <p class="text-lg"> {{ $post->description }} </p>
+                <div class="grid border p-2">
+                    <p class="text-md">
+                        Author: {{ $post->user->name }}
+                    </p>
+                    <p class="text-md">
+                        Published: {{ $post->created_at }}
+                    </p>
+                </div>
+                <div class="flex flex-row gap-2">
+                    <a class="bg-blue-200 max-w-[80px] border p-2 font-medium" href="{{ route('posts.update', $post) }}/edit">Update</a>
+                    <form action="{{ route('posts.destroy', $post) }}" method="POST" class="max-w-[80px]">
+                        @csrf
+                        @method('DELETE')
+                        <button class="bg-red-200 max-w-[80px] border p-2 font-medium">Delete</button>
+                    </form>
+                </div>
+
+            </div>
+        @endforeach
+    </section>
 </x-layout>
